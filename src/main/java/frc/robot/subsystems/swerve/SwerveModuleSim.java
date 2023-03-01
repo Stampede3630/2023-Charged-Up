2da2ee535a38da8c9d878145d8a8d1aa470c14a1
Vargoose84 <vargoose84@gmail.com>
2023-02-28 22:21:04 -0600

    fixed sim stuff... not working yet

--- src/main/java/frc/robot/subsystems/swerve/SwerveModuleSim.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveModuleSim.java
@@ -1,9 +1,10 @@
 package frc.robot.subsystems.swerve;
 
-import com.ctre.phoenix.motorcontrol.TalonFXSimCollection;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.sensors.CANCoder;
-import com.ctre.phoenix.sensors.CANCoderSimCollection;
+
+
+import com.ctre.phoenixpro.hardware.CANcoder;
+import com.ctre.phoenixpro.hardware.TalonFX;
+import com.ctre.phoenixpro.sim.TalonFXSimState;
 
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -22,16 +23,14 @@ public class SwerveModuleSim {
     public static final double kWheelDiameter = Units.inchesToMeters(4);
     public static final double kWheelCircumference = kWheelDiameter*Math.PI;
     // Simulation
-    private final TalonFXSimCollection driveMotorSim;
-    private final TalonFXSimCollection steerMotorSim;
-    private final CANCoderSimCollection steerSensorCANCoderSim;
+
     private final TalonFX driveMotor;
     private final TalonFX steerMotor;
-    private final CANCoder steerSensorCANCoder;
+    private final CANcoder steerSensorCANCoder;
     public static final SimpleMotorFeedforward kDriveFF = new SimpleMotorFeedforward( // real
-    0.2, // Voltage to break static friction
-    2.25, // Volts per meter per second
-    0.17 // Volts per meter per second squared
+    SwerveConstants.kS, // Voltage to break static friction
+    SwerveConstants.kV, // Volts per meter per second
+    SwerveConstants.kA // Volts per meter per second squared
     );
     // Steer feed forward
     public static final SimpleMotorFeedforward kSteerFF = new SimpleMotorFeedforward( // real
@@ -53,27 +52,27 @@ public class SwerveModuleSim {
         DCMotor.getFalcon500(1),
         SwerveConstants.STEERING_MOTOR_GEARING
     );
-    public SwerveModuleSim(TalonFX driveMotor, TalonFX steerMotor, CANCoder steerSensorCANCoder) {
+    public SwerveModuleSim(TalonFX driveMotor, TalonFX steerMotor, CANcoder steerSensorCANCoder) {
         this.driveMotor= driveMotor;
         this.steerMotor= steerMotor;
         this.steerSensorCANCoder = steerSensorCANCoder;
 
-        driveMotorSim = driveMotor.getSimCollection();
-        steerMotorSim = steerMotor.getSimCollection();
-        steerSensorCANCoderSim = steerSensorCANCoder.getSimCollection();
+
     }
 
     public void simulationPeriodic(double deltaTime){
+        TalonFXSimState driveSimState = driveMotor.getSimState();
+        TalonFXSimState steerSimState = steerMotor.getSimState();
         // apply our commanded voltage to our simulated physics mechanisms
-        double driveVoltage = driveMotorSim.getMotorOutputLeadVoltage();
+        double driveVoltage = driveSimState.getMotorVoltage();
         if(driveVoltage >= 0) driveVoltage = Math.max(0, driveVoltage-kSteerFF.ks);
         else driveVoltage = Math.min(0, driveVoltage+kSteerFF.ks);
-        driveWheelSim.setInputVoltage(driveVoltage);
+        driveSimState.setSupplyVoltage(driveVoltage);
 
-        double steerVoltage = steerMotorSim.getMotorOutputLeadVoltage();
+        double steerVoltage = steerSimState.getMotorVoltage();
         if(steerVoltage >= 0) steerVoltage = Math.max(0, steerVoltage-kSteerFF.ks);
         else steerVoltage = Math.min(0, steerVoltage+kSteerFF.ks);
-        steeringSim.setInputVoltage(steerVoltage);
+        steerSimState.setSupplyVoltage(steerVoltage);
         
         driveWheelSim.update(deltaTime);
         steeringSim.update(deltaTime);
@@ -81,33 +80,33 @@ public class SwerveModuleSim {
         // update our simulated devices with our simulated physics results
         double driveMotorVelocityNative = rotationsToVelocity(driveWheelSim.getAngularVelocityRPM()/60, SwerveConstants.DRIVE_MOTOR_GEARING);
         double driveMotorPositionDeltaNative = driveMotorVelocityNative*10*deltaTime;
-        driveMotorSim.setIntegratedSensorVelocity((int)driveMotorVelocityNative);
-        driveMotorSim.addIntegratedSensorPosition((int)(driveMotorPositionDeltaNative));
-        driveMotorSim.setSupplyCurrent(driveWheelSim.getCurrentDrawAmps()/2);
+        driveSimState.setRotorVelocity(driveMotorVelocityNative);
+        driveSimState.addRotorPosition(driveMotorPositionDeltaNative);
+        //driveSimState..setSupplyCurrent(driveWheelSim.getCurrentDrawAmps()/2);
 
         //SmartDashboard.putNumber("Steer Sim Model Velocity", steeringSim.getAngularVelocityRPM());
         double steerMotorVelocityNative = rotationsToVelocity(steeringSim.getAngularVelocityRPM()/60, SwerveConstants.STEERING_MOTOR_GEARING);
         double steerMotorPositionDeltaNative = steerMotorVelocityNative*10*deltaTime;
-        steerMotorSim.setIntegratedSensorVelocity((int)steerMotorVelocityNative);
-        steerMotorSim.addIntegratedSensorPosition((int)(steerMotorPositionDeltaNative));
+        steerSimState.setRotorVelocity(steerMotorVelocityNative);
+        steerSimState.addRotorPosition(steerMotorPositionDeltaNative);
         //System.out.println(steerMotorPositionDeltaNative);
-        steerMotorSim.setSupplyCurrent(steeringSim.getCurrentDrawAmps()/2);
-        steerSensorCANCoderSim.setVelocity((int)steerMotorVelocityNative);
-        steerSensorCANCoderSim.addPosition((int)(steerMotorPositionDeltaNative));
+        //steerMotorSim.setSupplyCurrent(steeringSim.getCurrentDrawAmps()/2);
+        steerSensorCANCoder.getSimState().setVelocity(steerMotorVelocityNative);
+        steerSensorCANCoder.getSimState().addPosition(steerMotorPositionDeltaNative);
         //steerSensorCANCoderSim.setVelocity((int)(rotationsToVelocity(steeringSim.getAngularVelocityRPM()/60, 1)*2));
         //steerSensorCANCoderSim.setRawPosition((int)(getIntegratedHeading().getDegrees()/360.0*4096));
 
-        driveMotorSim.setBusVoltage(RobotController.getBatteryVoltage());
-        steerMotorSim.setBusVoltage(RobotController.getBatteryVoltage());
+        //driveMotorSim.setBusVoltage(RobotController.getBatteryVoltage());
+        //steerMotorSim.setBusVoltage(RobotController.getBatteryVoltage());
         //steerEncoderSim.setBusVoltage(RobotController.getBatteryVoltage());
     }
 
-    public double getDriveCurrentDraw(){
-        return driveMotor.getSupplyCurrent();
-    }
-    public double getSteerCurrentDraw(){
-        return steerMotor.getSupplyCurrent();
-    }
+    // public double getDriveCurrentDraw(){
+    //     return driveMotor.getSupplyCurrent();
+    // }
+    // public double getSteerCurrentDraw(){
+    //     return steerMotor.getSupplyCurrent();
+    // }
 
     public static double positionToRotations(double nativePosition, double motorRotationsPerMechanismRotation){
         return nativePosition / 2048 / motorRotationsPerMechanismRotation;
