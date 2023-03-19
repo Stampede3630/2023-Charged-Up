f07a68b680d100322deb904da40a2793a92fa704
ToothbrushB <49379586+ToothbrushB@users.noreply.github.com>
2023-03-19 12:15:35 -0500

    gamepiece detection to use velocity

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -143,7 +143,8 @@ public class Intake extends SubsystemBase implements Loggable, Disableable, Enab
   }
 
   public Command waitUntilHaveGamePiece() {
-    return Commands.waitUntil(()-> m_debouncer.calculate(filteredCurrent > 60) || isLimitSwitchPressed())
-        .andThen(Commands.runOnce(() -> haveGamePiece = true));
+    return Commands.waitUntil(()-> m_intakeMotor.getSelectedSensorVelocity() > 15) // wait for spin up
+            .andThen(Commands.waitUntil(() -> m_debouncer.calculate(m_intakeMotor.getSelectedSensorVelocity() < 5) || isLimitSwitchPressed())) // wait for stopped TODO adjust the numbers
+            .andThen(Commands.runOnce(() -> haveGamePiece = true));
   }
 }
